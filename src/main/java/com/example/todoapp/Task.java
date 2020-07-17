d0bcbd1a5529e298a8d0313e495139a5ceb02ecd
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-07-17 22:58:55 +0200

    Task validation implemented

---
+++ src/main/java/com/example/todoapp/Task.java
@@ -0,0 +1,42 @@
+package com.example.todoapp;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private int id;
+    @NotBlank(message = "Task's description must be not empty")
+    private String description;
+    private boolean done;
+
+    public int getId() {
+        return id;
+    }
+
+    public Task(){}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        if(description!="")
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
