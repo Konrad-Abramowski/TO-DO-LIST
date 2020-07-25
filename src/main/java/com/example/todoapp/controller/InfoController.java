19713b484f8e49e1988e67f036e59aa4dce2d6a3
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-07-25 22:51:15 +0200

    Configuration profiles added

--- src/main/java/com/example/todoapp/controller/InfoController.java
+++ src/main/java/com/example/todoapp/controller/InfoController.java
@@ -1,23 +1,29 @@
 package com.example.todoapp.controller;
 
+import com.example.todoapp.TaskConfigurationProperties;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class InfoController {
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${my.prop}")
-    private String myProp;
+
+    private DataSourceProperties dataSourceProperties;
+    private TaskConfigurationProperties taskConfigurationProperties;
+
+    InfoController(final DataSourceProperties dataSourceProperties, final TaskConfigurationProperties taskConfigurationProperties) {
+        this.dataSourceProperties = dataSourceProperties;
+        this.taskConfigurationProperties = taskConfigurationProperties;
+    }
 
     @GetMapping("/info/url")
     String url(){
-        return url;
+        return dataSourceProperties.getUrl();
     }
 
     @GetMapping("/info/prop")
-    String myProp(){
-        return myProp;
+    boolean myProp(){
+        return taskConfigurationProperties.getTemplate().isAllowMultipleTasks();
     }
 }
