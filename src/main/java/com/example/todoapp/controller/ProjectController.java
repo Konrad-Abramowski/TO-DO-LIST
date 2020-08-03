31f7817e6bcbfc1eacf4bd672e2562144cdaaf53
Konrad-Abramowski <abramowski.konrad@gmail.com>
2020-08-03 22:49:57 +0200

    Projects view added

---
+++ src/main/java/com/example/todoapp/controller/ProjectController.java
@@ -0,0 +1,43 @@
+package com.example.todoapp.controller;
+
+import com.example.todoapp.logic.ProjectService;
+import com.example.todoapp.model.ProjectStep;
+import com.example.todoapp.model.projection.ProjectWriteModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/projects")
+class ProjectController {
+    private final ProjectService projectService;
+
+    ProjectController(final ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping
+    String showProjects(Model model) {
+        model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model){
+        projectService.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Dodano projekt!");
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+}
